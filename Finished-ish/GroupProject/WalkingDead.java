ef6cd2ea222571a45f7597d04c981cb4f8c03f1c
neric64 <neric@eagles.ewu.edu>
2014-03-18 09:40:44 -0700

    Updated tester

--- Finished-ish/GroupProject/WalkingDead.java
+++ Finished-ish/GroupProject/WalkingDead.java
@@ -2,37 +2,36 @@ package GroupProject;
 
 import java.util.ArrayList;
 
-
-public class test {
+public class WalkingDead {
 
 	public static void main(String[] args) {
 
-	    ArrayList<CharFactory.Character> battleList, heroList;
-	
+		ArrayList<CharFactory.Character> battleList, heroList;
+
 		Game walkingDead = new Game();
-		
+
 		heroList = walkingDead.Initialize();
 		CharFactory.Character character = heroList.get(0);
 		walkingDead.Move(character);
 		while (walkingDead.state != walkingDead.overgame)
 		{
-		    battleList = walkingDead.BuildMonster();
-		    if (battleList == null)
-		    {
-		    	walkingDead.GameOver();
-		    }
-		    else
-		    {
-		    	if (battleList.size() == 2)
-		    	{ }
-		    	else
-		    	{
-			    	for(int i = 0; i < heroList.size(); i++) {
+			battleList = walkingDead.BuildMonster();
+			if (battleList == null)
+			{
+				walkingDead.GameOver();
+			}
+			else
+			{
+				if (battleList.size() == 2)
+				{ }
+				else
+				{
+					for(int i = 0; i < heroList.size(); i++) {
 						battleList.add(heroList.get(i));
 					}
-		    	}
+				}
 				walkingDead.Encounter(battleList);
-		    }
+			}
 		}
 	}//end main
 
